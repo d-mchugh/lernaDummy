0ce522fd071748902ccc928d911dc6e434f2792d
Dean Mchugh <deanmchugh@Deans-MacBook-Pro.local>
2021-09-22 14:15:27 +1000

    added express to package2

--- packages/package2/index.js
+++ packages/package2/index.js
@@ -1,3 +1,14 @@
-const Add = require('package1');
+const express = require('express')
+const Add = require('package1')
+const app = express()
+const port = 3000
 
-console.log(Add(3, 6));
+const value = Add(3, 6);
+
+app.get('/', (req, res) => {
+  res.send(value.toString())
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
